9f8a8b61b66f5c6dd0fec358c0559a5469d7cc98
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-09 12:29:16 -0700

    Create plexFetch function

--- test/plex.js
+++ test/plex.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "Plex Connection",
     promise: new Promise((resolve, reject) => {
-        const config = require("../config");
-        const fetch = require("node-fetch");
-        fetch("http://" + config.plex.host + ":" + config.plex.port + "/?X-Plex-Token=" + config.plex.token)
+        const plexFetch = require("../plex/fetch");
+
+        plexFetch()
             .then(resolve)
             .catch(reject);
     })
