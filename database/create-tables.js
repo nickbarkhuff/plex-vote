106cbc27723a60b990762ed2f74b406915dd61b8
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-12 15:57:23 -0700

    Add UNIQUE column constraint to usernames

--- database/create-tables.js
+++ database/create-tables.js
@@ -7,7 +7,7 @@ const logErrors = (err) => {
 conn.query(`
     CREATE TABLE IF NOT EXISTS users(
         id       int          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        username varchar(255) NOT NULL,
+        username varchar(255) NOT NULL UNIQUE,
         password varchar(255) NOT NULL
     )
 `, logErrors);
