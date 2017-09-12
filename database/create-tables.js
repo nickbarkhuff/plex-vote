d96afe2174b4dec9b36bc157f743760017430876
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-12 15:35:55 -0700

    Auto-increment database table ID columns

--- database/create-tables.js
+++ database/create-tables.js
@@ -6,7 +6,7 @@ const logErrors = (err) => {
 
 conn.query(`
     CREATE TABLE IF NOT EXISTS users(
-        id       int          NOT NULL PRIMARY KEY,
+        id       int          NOT NULL AUTO_INCREMENT PRIMARY KEY,
         username varchar(255) NOT NULL,
         password varchar(255) NOT NULL
     )
@@ -14,7 +14,7 @@ conn.query(`
 
 conn.query(`
     CREATE TABLE IF NOT EXISTS votes(
-        id       int          NOT NULL PRIMARY KEY,
+        id       int          NOT NULL AUTO_INCREMENT PRIMARY KEY,
         user_id  int          NOT NULL,
         media_id varchar(255) NOT NULL,
         rating   int          NOT NULL
