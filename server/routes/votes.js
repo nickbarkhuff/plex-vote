8158b05b5464f724c6dcac13e6a3e4d95615ed64
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-12 17:04:47 -0700

    Add POST method to /votes API route

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -1,5 +1,19 @@
 const router = require("express").Router();
+const token = require("../../auth/token");
 
-router.get("/", (req, res) => res.send("Votes API"));
+router.get("/", (req, res) => {
+    res.end();
+});
+
+router.post("/", (req, res) => {
+    const userID = token.verify("user", req.body.jwt);
+
+    if(!userID){
+        res.status(401).send("UNAUTHORIZED");
+        return;
+    }
+
+    res.end();
+});
 
 module.exports = router;
