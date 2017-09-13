aaeef6dc1e088bde26a6f7208b14c11a8a1c676b
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-12 17:43:48 -0700

    Implement /votes POST API route

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -1,19 +1,52 @@
 const router = require("express").Router();
 const token = require("../../auth/token");
+const conn = require("../../database/connection");
+
+const RATING_MIN = 1;
+const RATING_MAX = 5;
 
 router.get("/", (req, res) => {
     res.end();
 });
 
 router.post("/", (req, res) => {
-    const userID = token.verify("user", req.body.jwt);
 
+    // Parameters
+    const userID = token.verify("user", req.body.jwt);
+    const mediaID = req.body.media;
+    const rating = parseInt(req.body.rating);
+
+    // Check that the user is authenticated
     if(!userID){
-        res.status(401).send("UNAUTHORIZED");
+        res.status(401).end();
         return;
     }
 
-    res.end();
+    // Check that all necessary parameters were supplied
+    if(!mediaID || !rating || rating < RATING_MIN || rating > RATING_MAX){
+        res.status(422).end();
+        return;
+    }
+
+    // Add vote
+    conn.query(`
+        INSERT INTO votes (user_id, media_id, rating)
+        VALUES (
+            ${userID},
+            "${mediaID}",
+            ${rating}
+        )
+    `, (err) => {
+
+        // Check for DB errors
+        if(err){
+            res.status(500).end();
+            return;
+        }
+
+        // Success
+        res.end();
+    });
 });
 
 module.exports = router;
