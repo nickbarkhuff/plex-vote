24a714cb345ac32cc2bdb1b22219d25b239b079a
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-12 20:24:23 -0700

    Fix Wilson score function
    Necessary parenthesis were missing

--- wilson-score.js
+++ wilson-score.js
@@ -3,5 +3,5 @@ const zSquared = Math.pow(zScore, 2);
 module.exports = (positive, total) => {
     if(!total) return 0;
     const average = positive/total;
-    return average + (zSquared / (2 * total)) - zScore * Math.sqrt((average * (1 - average) + zSquared / (4 * total) / total)) / (1 + zSquared / total);
+    return (average + (zSquared / (2 * total)) - zScore * Math.sqrt((average * (1 - average) + zSquared / (4 * total)) / total)) / (1 + zSquared / total);
 };
