fbb638b4f5327502a82d6a761a1e4be19f7e80f6
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-14 04:40:42 -0700

    Add files for main client-side routes

---
+++ client/App/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Login extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return <div>Login</div>;
+    }
+}
+
+export default Login;
