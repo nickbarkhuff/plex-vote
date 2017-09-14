f357a3ebc326755c7dd792bfb2683714449e5b5f
Nick Barkhuff <npbarkhuff@gmail.com>
2017-09-14 04:44:36 -0700

    Implement main client-side routes in main app

--- client/App/index.js
+++ client/App/index.js
@@ -1,7 +1,37 @@
 import React from "react";
+import {Switch, Route, Redirect} from "react-router-dom";
 
-const App = () => (
-    <div>Example component.</div>
-);
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+import Results from "./Results";
+
+class App extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            loggedIn: false
+        };
+    }
+
+    render(){
+        const protect = (jsx) => {
+            if(!this.state.loggedIn)
+                return <Redirect to="/login"/>;
+            else
+                return jsx;
+        };
+
+        return (
+            <Switch>
+                <Route path="/login" render={() => <Login ps={this.ps}/>}/>
+                <Route path="/register" render={() => <Register ps={this.ps}/>}/>
+                <Route path="/results" render={() => <Results ps={this.ps}/>}/>
+                <Route path="/" render={() => protect(<Home ps={this.ps}/>)}/>
+            </Switch>
+        );
+    }
+}
 
 export default App;
